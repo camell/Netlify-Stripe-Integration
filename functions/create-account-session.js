afd64db0e77a5120178b2404b408e4973a4ebe1b
Chris Amell <camell@users.noreply.github.com>
2024-07-16 15:14:58 -0700

    Update create-account-session.js

--- functions/create-account-session.js
+++ functions/create-account-session.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type'
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
   };
 
   if (event.httpMethod === 'OPTIONS') {
