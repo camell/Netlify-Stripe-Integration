b82a536aa37662e588a8f417d087e57d01cb0d14
Chris Amell <camell@users.noreply.github.com>
2024-07-16 15:42:27 -0700

    Update create-account-session.js

--- functions/create-account-session.js
+++ functions/create-account-session.js
@@ -35,6 +35,7 @@ exports.handler = async (event, context) => {
   } catch (error) {
     return {
       statusCode: 500,
+      headers,
       body: JSON.stringify({ error: error.message }),
     };
   }
