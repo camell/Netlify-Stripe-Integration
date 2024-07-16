4bd967d4f104477888c478b7bc23118de9349fd6
Chris Amell <camell@users.noreply.github.com>
2024-07-16 14:46:29 -0700

    Update create-account-session.js

--- functions/create-account-session.js
+++ functions/create-account-session.js
@@ -4,7 +4,6 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event, context) => {
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
   };
 
